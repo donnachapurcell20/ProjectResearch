65c52bc1f181f7bd955575be41b162a4d2b452bb
Donnacha Purcell <donnachapurcell20@gmail.com>
2022-01-09 18:06:01 +0000

    Created class named "VideoGames" and created variables with getters and setters

---
+++ src/VideoGames.java
@@ -0,0 +1,58 @@
+
+public class VideoGames
+{
+    private int id;
+    private String game;
+    private String genre;
+    private String developer;
+    private String publisher;
+    private String platform;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGame() {
+        return game;
+    }
+
+    public void setGame(String game) {
+        this.game = game;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+    public void setDeveloper(String developer) {
+        this.developer = developer;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+}
